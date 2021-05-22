1754c1b831ae674d2070e15c3ec2fb7b19c4188d
Daniil <krivelevdaniil@gmail.com>
2021-05-22 18:42:30 +0700

    Initial Commit

---
+++ NTR-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NTR-Task
+//
+//  Created by KriDan on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
